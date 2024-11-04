8c8fa02b07b28bd924e9555a48824ee2ad44d429
Sam Robbins <93335264+OHeckIsThatSam@users.noreply.github.com>
2024-11-04 16:40:32 +0000

    Added creation scripts for database and tables

---
+++ Database/5 - User.sql
@@ -0,0 +1,57 @@
+USE [AML]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 04/11/2024 16:26:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[TokenID] [int] NOT NULL,
+	[AddressID] [int] NOT NULL,
+	[RoleID] [int] NOT NULL,
+	[Username] [varchar](max) NOT NULL,
+	[Password] [varchar](max) NOT NULL,
+	[Email] [varchar](max) NOT NULL,
+	[FirstName] [varchar](max) NOT NULL,
+	[LastName] [varchar](max) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Verified] [bit] NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_Created]  DEFAULT (getdate()) FOR [Created]
+GO
+
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_Verified]  DEFAULT ((0)) FOR [Verified]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Address] FOREIGN KEY([AddressID])
+REFERENCES [dbo].[Address] ([ID])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Address]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Role] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[Role] ([ID])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Role]
+GO
+
+ALTER TABLE [dbo].[User]  WITH NOCHECK ADD  CONSTRAINT [FK_User_Token] FOREIGN KEY([TokenID])
+REFERENCES [dbo].[Token] ([ID])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Token]
+GO
+
+
---
+++ Database/README.md
@@ -0,0 +1,7 @@
+# Database Initialisation
+
+Follow this process to set up the database for this project:
+
+1. If databse (AML) is not yet created run file 1 else skip
+2. Run SQL in numbered file order to create tables. Order matters for defined relationships and constraints
+3. If a table already exsits CREATE TABLE will fail. DROP any existing tables if the tables have been previously created before
