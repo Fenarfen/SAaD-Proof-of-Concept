004b271c4db825b43a54e8da29766392a6b45414
OHeckIsThatSam <sam@scrooby.net>
2024-11-04 17:42:54 +0000

    Docker container intialisation

--- InventoryAPI/obj/Debug/net8.0/InventoryAPI.AssemblyInfo.cs
+++ InventoryAPI/obj/Debug/net8.0/InventoryAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec6e5d205d288057b66c653252ac5a7f3a03e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
