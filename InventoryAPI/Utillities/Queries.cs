a1ad182eb4ddd9d36063d2c9b707c8f9d2ef5dd1
OHeckIsThatSam <sam@scrooby.net>
2024-11-06 15:39:45 +0000

    Updated media queries to not return media currently being transfered

--- InventoryAPI/Utillities/Queries.cs
+++ InventoryAPI/Utillities/Queries.cs
@@ -3,25 +3,39 @@
 static public class Queries
 {
     public const string GetAllMedia = @"
-        SELECT m.ID, m.Title, m.Released, m.Author, m.[Type], 
-	    m.BranchID AS ID, b.[Name], b.Opened, 
-        b.AddressID AS ID, a.FirstLine, a.SecondLine, a.PostCode, 
-        a.CityID AS ID, c.[Name]
-        FROM AML.dbo.Media m
-        INNER JOIN Branch b ON m.BranchID = b.ID
-        INNER JOIN [Address] a ON b.AddressID = a.ID
-        INNER JOIN City c ON a.CityID = c.ID";
-
-    public const string GetMediaByCity = @"
-        SELECT m.ID, m.Title, m.Released, m.Author, m.[Type], 
-        m.BranchID AS ID, b.[Name], b.Opened, 
-        b.AddressID AS ID, a.FirstLine, a.SecondLine, a.PostCode, 
-        a.CityID AS ID, c.[Name]
+        SELECT 
+        	m.ID, m.Title, m.Released, m.Author, m.[Type], 
+        	m.BranchID AS ID, b.[Name], b.Opened, 
+        	b.AddressID AS ID, a.FirstLine, a.SecondLine, a.PostCode, 
+        	a.CityID AS ID, c.[Name]
         FROM AML.dbo.Media m
         INNER JOIN Branch b ON m.BranchID = b.ID
         INNER JOIN [Address] a ON b.AddressID = a.ID
         INNER JOIN City c ON a.CityID = c.ID
-        WHERE a.CityID = @CityID";
+        LEFT JOIN ( 
+        	SELECT mt.MediaID 
+        	FROM MediaTransfer mt
+        	WHERE mt.Completed IS NULL
+        ) mt ON mt.MediaID = m.ID
+        WHERE mt.MediaID IS NULL;";
+
+    public const string GetMediaByCity = @"
+        SELECT 
+        	m.ID, m.Title, m.Released, m.Author, m.[Type], 
+        	m.BranchID AS ID, b.[Name], b.Opened, 
+        	b.AddressID AS ID, a.FirstLine, a.SecondLine, a.PostCode, 
+        	a.CityID AS ID, c.[Name]
+        FROM AML.dbo.Media m
+        INNER JOIN Branch b ON m.BranchID = b.ID
+        INNER JOIN [Address] a ON b.AddressID = a.ID
+        INNER JOIN City c ON a.CityID = c.ID
+        LEFT JOIN ( 
+        	SELECT mt.MediaID 
+        	FROM MediaTransfer mt
+        	WHERE mt.Completed IS NULL
+        ) mt ON mt.MediaID = m.ID
+        WHERE mt.MediaID IS NULL 
+        AND a.CityID = 1;";
 
     public const string GetMediaSplitOn = "ID,ID,ID";
 }
