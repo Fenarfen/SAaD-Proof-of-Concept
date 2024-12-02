3829af7220bd15469f432deec8974ec17a93828c
finegreat <c1027120@my.shu.ac.uk>
2024-12-02 04:31:08 +0000

    functionality moved from amlmonitor to reportapi

--- AMLMonitor/Models/Reports/MediaUsageReport.cs
+++
@@ -1,9 +0,0 @@
-namespace AMLMonitor.Models.Reports
-{
-    public class MediaUsageReport
-    {
-        public string MediaTitle { get; set; }
-        public string MediaType { get; set; }
-        public int BorrowedCount { get; set; }
-    }
-}
